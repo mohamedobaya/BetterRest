e6c0f39182f90d5e8e7ac8f885e0d20388cf9e59
Mohamed Obaya <mohamed.ali.obaya@gmail.com>
2025-04-30 13:14:30 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mohamed Obaya on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
