f5b91bec967c1105e6e346a010ef9c65f6a71243
Mohamed Obaya <mohamed.ali.obaya@gmail.com>
2025-05-01 13:19:39 +0300

    coreML connected with the app

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Mohamed Obaya on 30/04/2025.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationStack {
@@ -28,15 +32,39 @@ struct ContentView: View {
                     .font(.headline)
                 Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
             }
-            .navigationTitle("BetterRest ⏾ ")
+            .navigationTitle("BetterRest ⏾")
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK"){}
+            } message: {
+                Text("\(alertMessage)")
             }
         }
     }
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            let hour = (components.hour ?? 0) * 3600
+            let minute  = (components.minute ?? 0) * 60
+            
+            let predection = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - predection.actualSleep
+   
+            alertTitle = "Your idle bed time is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+//            print(alertMessage)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showAlert = true
     }
 }
 
